20cb7d3f4215a1821ea000b2c4892c577cf5056f
Kasidit Nusitchaiyakan <nusitchaiyak@seattleu.edu>
2020-09-23 08:29:07 -0700

    created scaffolding

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MvcMovie.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace testweb
 {
@@ -24,9 +26,15 @@ namespace testweb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
+
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -54,4 +62,7 @@ namespace testweb
             });
         }
     }
+    
 }
+
+
